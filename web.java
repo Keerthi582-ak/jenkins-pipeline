c19ebc131cd1fc3b104ced73875c59cd539028f8
amudalakeerthi9@gmail.com <amudalakeerthi9@gmail.com>
2025-05-24 17:41:01 +0000

    latest

--- web.java
+++ web.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Keerthi How are you");
+        System.out.println("Hello Keerthi How are you? Welcome to jenkins");
     }
 }
